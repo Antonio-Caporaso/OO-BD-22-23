acdfbef624edfeec057ce41af18ebdc089cc50fa
Giordi9902 <giorgio99difusco@gmail.com>
2022-12-29 18:26:02 +0100

    Aggiunto package per connessione al database

--- Codice/src/BusinessLogic.java
+++ Codice/src/BusinessLogic.java
@@ -1,8 +1,23 @@
 import Conferenza.*;
 import Organizzazione.*;
+import database.*;
+
+import java.sql.Connection;
 public class BusinessLogic {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Questa è la Business Logic!");
+		
+		//Verifica connessione al database
+		Connection conn = null;
+        DBConnection dbConnection = DBConnection.getDbconnection();
+        conn = dbConnection.getConnection();
+
+        if(conn == null){
+            System.out.println("Connessione non riuscita");
+            System.exit(0);
+        }
+        System.out.println("Connessione OK");
 	}
 }
