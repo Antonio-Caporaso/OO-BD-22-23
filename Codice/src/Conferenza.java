d3e3d4b133542addf27aac53dfeca9d9d53baaf3
Giordi9902 <giorgio99difusco@gmail.com>
2022-12-29 12:24:48 +0100

    Aggiunta metodi getter e setter per nomeConferenza

--- Codice/src/Conferenza.java
+++ Codice/src/Conferenza.java
@@ -1,5 +1,14 @@
 
 public class Conferenza {
+
 	private String nomeConferenza;
+
+	public String getNomeConferenza() {
+		return nomeConferenza;
+	}
+
+	public void setNomeConferenza(String nomeConferenza) {
+		this.nomeConferenza = nomeConferenza;
+	}
 	
 }
