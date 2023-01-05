36fa94c84574b679ef45568dbec209ea558f3246
Giordi9902 <giorgio99difusco@gmail.com>
2023-01-05 15:45:22 +0100

    Modifiche BusinessLogic

--- Codice/src/BusinessLogic.java
+++ Codice/src/BusinessLogic.java
@@ -1,19 +1,14 @@
-import entity.conferenza.*;
 import dbConfig.*;
-import entity.organizzazione.*;
-
-import java.sql.Connection;
+import java.sql.*;
 public class BusinessLogic {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Questa è la Business Logic!");
-		
 		//Verifica connessione al database
 		Connection conn = null;
         DBConnection dbConnection = DBConnection.getDbconnection();
         conn = dbConnection.getConnection();
-
         if(conn == null){
             System.out.println("Connessione non riuscita");
             System.exit(0);
