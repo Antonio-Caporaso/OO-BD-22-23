0953deda6e8a55b9ca7da4c4de207a25e5b922c6
Antonio Caporaso <anto.caporaso00@gmail.com>
2023-01-09 12:41:10 +0100

    Update ProgrammaSessione.java
    primi attributi e metodi,  con riferimento ad altre classi

--- Codice/src/entity/conferenza/ProgrammaSessione.java
+++ Codice/src/entity/conferenza/ProgrammaSessione.java
@@ -1,5 +1,33 @@
 package entity.conferenza;
 
+import java.util.Set;
+
 public class ProgrammaSessione extends Programma {
+
+    private Sessione sessione;
+    private Set<Intervento> interventi;
+    private Set<Intervallo> intervalli;
+    //keynote speaker?
+    public Sessione getSessione() {
+        return sessione;
+    }
+
+    public Set<Intervento> getInterventi() {
+        return interventi;
+    }
+
+    public Set<Intervallo> getIntervalli() {
+        return intervalli;
+    }
+
+    public void setSessione(Sessione sessione) {
+        this.sessione = sessione;
+    }
+    public void addIntervento(Intervento intervento) {
+        interventi.add(intervento);
+    }
+    public void addIntervallo(Intervallo intervallo) {
+        intervalli.add(intervallo);
+    }
 
 }
