a1029ec768538ceae19fe8dd8e52719dbabfc782
Giordi9902 <giorgio99difusco@gmail.com>
2023-01-21 01:13:22 +0100

    Add LoginController

---
+++ Codice/src/View/Main.java
@@ -0,0 +1,35 @@
+package View;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+public class Main extends Application {
+    public static void main(String[]args){
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        //Definiamo oggetti delle classi modello da passare ai vari controller
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+            HBox root = (HBox) loader.load();
+
+            // Ottengo un riferimento al controller
+            LoginController controller = loader.getController();
+            Scene scene = new Scene(root);
+
+            stage.getIcons().add(new Image("View/Resources/Logo.jpg"));
+            stage.setTitle("Conference Manager");
+            stage.setScene(scene);
+            stage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
