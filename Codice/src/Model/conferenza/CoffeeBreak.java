4bfd2aeb0ccc114e1f1fc7322a1a5d1b5661ac7c
Giordi9902 <giorgio99difusco@gmail.com>
2023-01-23 13:24:39 +0100

    Aggiunta costruttore

--- Codice/src/Model/conferenza/CoffeeBreak.java
+++ Codice/src/Model/conferenza/CoffeeBreak.java
@@ -1,4 +1,9 @@
 package Model.conferenza;
 
+import java.util.Date;
+
 public class CoffeeBreak extends Intervallo{
+    public CoffeeBreak(Date oraInizio, Date oraFine) {
+        super(oraInizio, oraFine);
+    }
 }
--- Codice/src/Model/conferenza/Pranzo.java
+++ Codice/src/Model/conferenza/Pranzo.java
@@ -1,4 +1,9 @@
 package Model.conferenza;
 
+import java.util.Date;
+
 public class Pranzo extends Intervallo {
+    public Pranzo(Date oraInizio, Date oraFine) {
+        super(oraInizio, oraFine);
+    }
 }
