789fb54a5054bb6215165eabb1157546a2c50a1f
Antonio Caporaso <anto.caporaso00@gmail.com>
2023-01-27 22:23:48 +0100

    Update LandingController.java
    Gestione bottoni menu

--- Codice/src/Controller/LandingController.java
+++ Codice/src/Controller/LandingController.java
@@ -6,19 +6,59 @@ import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class LandingController {
-    @FXML
-    private Button backButton;
-@FXML
-    void backButtonOnAction(ActionEvent event) {
 
+    @FXML
+    private Button creaConferenzaButton;
+
+    @FXML
+    private Button gestisciConferenzaButton;
+
+    @FXML
+    private Button visualizzaConferenza;
+//    private NavigationController navigationController = NavigationController.getInstance();
+//
+//    @FXML
+//    void creaConferenzaOnAction(ActionEvent event) {
+//        loadScene("/View/FXML/Registrazione.fxml", creaConferenzaButton);
+//    }
+
+    @FXML
+    void creaConferenzaOnAction(ActionEvent event) {
         try {
-            /*Si chiama l'evento di navigazione passando l'FXML della scena di login.
-            Si recupera la finestra principale corrente utilizzando il metodo getScene().getWindow()
-             e la passa all'istanza della classe di navigazione utilizzando il metodo setStage().*/
-            NavigationController.getInstance().setStage((Stage) backButton.getScene().getWindow());
-            NavigationController.getInstance().loadScene("/View/FXML/Registrazione.fxml");
-        }catch (Exception e){
+            NavigationController.getInstance().setStage((Stage) creaConferenzaButton.getScene().getWindow());
+            NavigationController.getInstance().loadScene("/View/FXML/CreaConferenza.fxml");
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    void gestisciConferenzaOnAction(ActionEvent event) {
+        try {
+            NavigationController.getInstance().setStage((Stage) gestisciConferenzaButton.getScene().getWindow());
+            NavigationController.getInstance().loadScene("/View/FXML/GestisciConferenza.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void visualizzaConferenzaOnAction(ActionEvent event) {
+        try {
+            NavigationController.getInstance().setStage((Stage) visualizzaConferenza.getScene().getWindow());
+            NavigationController.getInstance().loadScene("/View/FXML/VisualizzaConferenza.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    void initialize() {
+        assert creaConferenzaButton != null : "fx:id=\"creaConferenzaButton\" was not injected: check your FXML file 'Landing.fxml'.";
+        assert gestisciConferenzaButton != null : "fx:id=\"gestisciConferenzaButton\" was not injected: check your FXML file 'Landing.fxml'.";
+        assert visualizzaConferenza != null : "fx:id=\"visualizzaConferenza\" was not injected: check your FXML file 'Landing.fxml'.";
+
+
     }
 }
