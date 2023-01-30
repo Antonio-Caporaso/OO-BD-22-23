27d821f1cbfae4219e667cb06b7f3e8e0d069833
Giordi9902 <giorgio99difusco@gmail.com>
2023-01-30 16:06:08 +0100

    Rimozione commenti in DBConnection.java

--- Codice/src/DbConfig/DBConnection.java
+++ Codice/src/DbConfig/DBConnection.java
@@ -14,15 +14,11 @@ public class DBConnection {
             dbcon = new DBConnection();
         return dbcon;
     }
-
-    // metodo per ottenere la connessione
     public Connection getConnection(){
         String pwd = "mandarino";
         try{
             if(conn == null || conn.isClosed()){
-                //Registrazione del Driver
                 Class.forName("org.postgresql.Driver");
-                // Chiama il Driver Manager e chiedi la connessione postgres
                 conn = DriverManager.getConnection("jdbc:postgresql://database-oobd.ct0ckvilfzkl.eu-central-1.rds.amazonaws.com","Master_User",pwd);
             }
         }catch (SQLException | ClassNotFoundException throwables){
