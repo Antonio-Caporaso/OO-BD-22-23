0b062e1252bd93e31dfc2cd1153e4babf9057fe2
Antonio Caporaso <anto.caporaso00@gmail.com>
2023-02-01 16:09:25 +0100

    Update registrazione
    Aggiunta di tutti i campi necessari alla registrazione (layout da ridiscutere)

--- Codice/src/Controller/RegistrazioneController.java
+++ Codice/src/Controller/RegistrazioneController.java
@@ -2,38 +2,92 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
-public class RegistrazioneController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegistrazioneController implements Initializable{
+
     @FXML
-    private Button backButton1;
+    private Button backButton;
+
     @FXML
-    private Button landingButton;
+    private Label cognomeLabel;
+
+    @FXML
+    private TextField cognomeTextField;
+
+    @FXML
+    private Label confermaPasswordLabel;
+
+    @FXML
+    private PasswordField confermaPasswordTextField;
+
+    @FXML
+    private Button confirmButton;
+
+    @FXML
+    private Label emailLabel;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private Label nomeLabel;
+
+    @FXML
+    private TextField nomeTextField;
+
+    @FXML
+    private Label passwordLabel;
+
+    @FXML
+    private PasswordField passwordTextField;
+
+    @FXML
+    private ChoiceBox<String> titoloChoiceBox;
+
+    @FXML
+    private Label titoloLabel;
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    private TextField usernameTextField;
+    private String[]titoli={"Dr.","Dott.","Dott.ssa","Prof.","Prof.ssa","Ing.","Sig","Sig.ra","Altro"};
+
     @FXML
     void backButtonOnAction(ActionEvent event) {
-
         try {
             /*Si chiama l'evento di navigazione passando l'FXML della scena di login.
             Si recupera la finestra principale corrente utilizzando il metodo getScene().getWindow()
              e la passa all'istanza della classe di navigazione utilizzando il metodo setStage().*/
-            NavigationController.getInstance().setStage((Stage) backButton1.getScene().getWindow());
+            NavigationController.getInstance().setStage((Stage) backButton.getScene().getWindow());
             NavigationController.getInstance().loadScene("/View/FXML/Login.fxml");
         }catch (Exception e){
             e.printStackTrace();
         }
     }
     @FXML
-    void landingButtonOnAction(ActionEvent event) {
+    void confirmButtonOnAction(ActionEvent event) {
 
         try {
             /*Si chiama l'evento di navigazione passando l'FXML della scena di login.
             Si recupera la finestra principale corrente utilizzando il metodo getScene().getWindow()
              e la passa all'istanza della classe di navigazione utilizzando il metodo setStage().*/
-            NavigationController.getInstance().setStage((Stage) landingButton.getScene().getWindow());
+            NavigationController.getInstance().setStage((Stage) confirmButton.getScene().getWindow());
             NavigationController.getInstance().loadScene("/View/FXML/Landing.fxml");
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+    //serve per inizializzare il controller dopo che l'elemento di root è stato inizializzato
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        titoloChoiceBox.getItems().addAll(titoli);
     }
 }
