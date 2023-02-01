73dd042e5b8322ca2525e66ddc9fa1e6b49f664a
Antonio Caporaso <anto.caporaso00@gmail.com>
2023-02-01 17:50:30 +0100

    Update to login

--- Codice/src/Controller/LoginController.java
+++ Codice/src/Controller/LoginController.java
@@ -6,12 +6,21 @@ package Controller;
 import DAO.UtenteDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
-public class LoginController {
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable{
     //private Stage primaryStage;
     @FXML
     private Label errorLabel;
@@ -23,6 +32,11 @@ public class LoginController {
     private TextField usernameTextField; // Value injected by FXMLLoader
     @FXML // fx:id="passwordTextField"
     private PasswordField passwordTextField; // Value injected by FXMLLoader
+    @FXML
+    private MediaView mediaView;
+    private Media media;
+    private File file;
+    private MediaPlayer mediaPlayer;
     private UtenteDAO userdao;
     @FXML
     void loginButtonOnAction(ActionEvent event) {
@@ -61,5 +75,15 @@ public class LoginController {
         assert usernameTextField != null : "fx:id=\"usernameTextField\" was not injected: check your FXML file 'Login.fxml'.";
         assert passwordTextField != null : "fx:id=\"passwordTextField\" was not injected: check your FXML file 'Login.fxml'.";
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        file= new File ("Codice/src/View/Resources/Scientists.mp4");
+        media= new Media(file.toURI().toString());
+        mediaPlayer=new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
     }
 }
