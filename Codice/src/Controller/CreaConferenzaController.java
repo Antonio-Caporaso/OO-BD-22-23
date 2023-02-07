624e23b34026cb9adca718f01495a549e9a6739e
Giordi9902 <giorgio99difusco@gmail.com>
2023-02-07 11:48:11 +0100

    Modifica finestra per creazione conferenza

--- Codice/src/Controller/CreaConferenzaController.java
+++ Codice/src/Controller/CreaConferenzaController.java
@@ -1,76 +1,79 @@
 package Controller;
 
-import DAO.ConferenzaDao;
-import DAO.SedeDao;
-import Model.conferenza.Sede;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.SubScene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TitledPane;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 
 public class CreaConferenzaController {
-
     @FXML
-    private TitledPane DettagliTitle;
-
+    private Button inserisciDettagli;
     @FXML
-    private TitledPane PromotoriTitle;
-
+    private Button inserisciEnti;
     @FXML
-    private TitledPane SedeTitle;
-
+    private Button inserisciSede;
     @FXML
-    private ImageView image;
+    private Button inserisciSessioni;
     @FXML
-    private ChoiceBox<String> sediPicker;
-    private SedeDao sededao;
+    private Button inserisciSponsor;
     @FXML
-    private Button newSedeButton;
-    private ConferenzaDao conferenzaDAO;
+    private SubScene subscene;
     @FXML
-    private SubScene newSedeScene;
-    @FXML
-    void DettagliTitleOnAction(MouseEvent event){
-        Image img = new Image("View/Resources/conference.png");
-        image.setImage(img);
-    }
-    @FXML
-    void PromotoriTitleOnAction(MouseEvent event) {
-        Image img = new Image("View/Resources/meeting.png");
-        image.setImage(img);
-    }
-
-    @FXML
-    void SedeTitleOnAction(MouseEvent event) {
-        Image img = new Image("View/Resources/city.png");
-        image.setImage(img);
-    }
-    @FXML
-    void newSedeButtonOnAction(ActionEvent event){
+    void inserisciDettagliOnAction(ActionEvent event) {
         Parent loader = null;
         try {
-            loader = FXMLLoader.load(getClass().getResource("../View/FXML/NuovaSede.fxml"));
+            loader = FXMLLoader.load(getClass().getResource("../View/FXML/InserisciDettagliConferenza.fxml"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        newSedeScene.setRoot(loader);
+        subscene.setRoot(loader);
     }
     @FXML
-    void initialize() {
-        sededao = new SedeDao();
-        assert DettagliTitle != null : "fx:id=\"DettagliTitle\" was not injected: check your FXML file 'CreaConferenza.fxml'.";
-        assert PromotoriTitle != null : "fx:id=\"PromotoriTitle\" was not injected: check your FXML file 'CreaConferenza.fxml'.";
-        assert SedeTitle != null : "fx:id=\"SedeTitle\" was not injected: check your FXML file 'CreaConferenza.fxml'.";
-        assert image != null : "fx:id=\"image\" was not injected: check your FXML file 'CreaConferenza.fxml'.";
-        sediPicker.getItems().setAll(sededao.retrieveNomeSedi());
+    void inserisciEntiOnAction(ActionEvent event) {
+        Parent loader = null;
+        try {
+            loader = FXMLLoader.load(getClass().getResource("../View/FXML/InserisciEnti.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        subscene.setRoot(loader);
     }
+    @FXML
+    void inserisciSedeOnAction(ActionEvent event) {
+        Parent loader = null;
+        try {
+            loader = FXMLLoader.load(getClass().getResource("../View/FXML/InserisciSede.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        subscene.setRoot(loader);
+    }
+
+    @FXML
+    void inserisciSessioniOnAction(ActionEvent event) {
+        Parent loader = null;
+        try {
+            loader = FXMLLoader.load(getClass().getResource("../View/FXML/InserisciSessioni.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        subscene.setRoot(loader);
+    }
+
+    @FXML
+    void inserisciSponsorOnAction(ActionEvent event) {
+        Parent loader = null;
+        try {
+            loader = FXMLLoader.load(getClass().getResource("../View/FXML/InserisciSponsor.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        subscene.setRoot(loader);
+    }
+
 }
