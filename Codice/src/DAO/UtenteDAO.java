3c4bad32264aaa1439eabd448f29d1fbea50782b
Giordi9902 <giorgio99difusco@gmail.com>
2023-02-08 14:06:53 +0100

    Modifica metodo saveUtente in UtenteDAO.java

--- Codice/src/DAO/UtenteDAO.java
+++ Codice/src/DAO/UtenteDAO.java
@@ -34,37 +34,19 @@ public class UtenteDAO {
         }
         return user;
     }
-    public String getPasswordByUsername(String username){
-        dbCon = DBConnection.getDBconnection();
-        conn = dbCon.getConnection();
-        String pwd = null;
-        try{
-            PreparedStatement stm = conn.prepareStatement("SELECT pwd FROM utente WHERE username=?");
-            stm.setString(1, username);
-            ResultSet result = stm.executeQuery();
-            while(result.next()) {
-                pwd = result.getString(1);
-            }
-            result.close();
-            stm.close();
-        }catch (SQLException throwables){
-            throwables.printStackTrace();
-        }
-        return pwd;
-    }
     public void saveUtente(Utente utente) throws UtentePresenteException{
         dbCon = DBConnection.getDBconnection();
         conn = dbCon.getConnection();
         try{
             if(userIsPresentByUsername(utente.getUsername())){
-                PreparedStatement query = conn.prepareStatement("INSERT INTO utente VALUES (default,?,?,?,?,?,?)");
+                PreparedStatement query = conn.prepareStatement("INSERT INTO utente VALUES (default,?,?,?,?,?,?,?)");
                 query.setString(1,utente.getPassword());
                 query.setString(2,utente.getNome());
                 query.setString(3,utente.getCognome());
                 query.setString(4,utente.getTitolo());
                 query.setString(5, utente.getEmail());
                 query.setString(6,utente.getUsername());
-                //query.setString(7,utente.getIstituzione());
+                query.setString(7,utente.getIstituzione());
                 query.executeUpdate();
             }else{
                 throw new UtentePresenteException();
