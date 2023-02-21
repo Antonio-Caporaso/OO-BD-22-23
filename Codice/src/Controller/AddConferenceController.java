ede5e1d4af0584619313d6d09e4c1ee40b178034
Giordi9902 <giorgio99difusco@gmail.com>
2023-02-21 10:50:33 +0100

    Commit

--- Codice/src/Controller/AddConferenceController.java
+++ Codice/src/Controller/AddConferenceController.java
@@ -5,8 +5,16 @@ import Model.Conferenze.Conferenza;
 import Model.Utente;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Date;
 import java.time.LocalDate;
@@ -60,9 +68,13 @@ public class AddConferenceController implements Initializable{
         ConferenzaDao dao = new ConferenzaDao();
         try {
             dao.saveConferenza(c);
+            openDialogWindow();
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+    public void openDialogWindow() {
+
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
---
+++ Codice/src/Controller/AddedConferenceDialogController.java
@@ -0,0 +1,17 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AddedConferenceDialogController {
+
+    @FXML
+    private Button okButton;
+
+    @FXML
+    void okButtonOnAction(ActionEvent event) {
+
+    }
+
+}
