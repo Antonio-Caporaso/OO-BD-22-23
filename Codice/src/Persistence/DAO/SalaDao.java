3d329dec46c06ab7503cb6c48dd292e49418b5a0
Antonio Caporaso <anto.caporaso00@gmail.com>
2023-03-03 16:15:04 +0100

    Aggiunta funzioni
    aggiunte funzioni retrieveNomeSalaBySedeID e retreiveSalaBySedeIdAndNomeSala

--- Codice/src/Persistence/DAO/SalaDao.java
+++ Codice/src/Persistence/DAO/SalaDao.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SalaDao {
     private DBConnection dbcon;
@@ -29,4 +31,36 @@ public class SalaDao {
         }
         return s;
     }
+    public List<String> retrieveNomeSalaBySedeID(int idsede) throws SQLException {
+        dbcon = DBConnection.getDBconnection();
+        conn = dbcon.getConnection();
+        String query = "SELECT nomesala FROM sala WHERE idsede = ?";
+        PreparedStatement stm = conn.prepareStatement(query);
+        stm.setInt(1,idsede);
+        List<String> salaNames = new ArrayList<>();
+        ResultSet rs = stm.executeQuery();
+        while (rs.next()) {
+            salaNames.add(rs.getString(1));
+        }
+        return salaNames;
+    }
+    public Sala retreiveSalaBySedeIdAndNomeSala(int idsede, String nomeSala) throws SQLException{
+        dbcon = DBConnection.getDBconnection();
+        conn = dbcon.getConnection();
+        String query = "SELECT * FROM sala WHERE idsede = ? AND nomesala= ?";
+        PreparedStatement stm = conn.prepareStatement(query);
+        stm.setInt(1,idsede);
+        stm.setString(2,nomeSala);
+        Sala s = new Sala();
+        SedeDao sedeDao = new SedeDao();
+        ResultSet rs = stm.executeQuery();
+        while(rs.next()){
+            s.setSalaID(rs.getInt(1));
+            s.setSede(sedeDao.retrieveSedeByID(rs.getInt(2)));
+            s.setCapacity(rs.getInt(3));
+            s.setNomeSala(rs.getString(4));
+        }
+        return s;
+    }
+
 }
