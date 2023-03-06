19e0c0b88becc86be50bf8a5d667ef6893d78a32
Giordi9902 <giorgio99difusco@gmail.com>
2023-03-06 22:18:44 +0100

    Modifica edit conferenza

---
+++ Codice/src/View/Controller/AddProgrammaController.java
@@ -0,0 +1,110 @@
+package View.Controller;
+
+import Persistence.Entities.Conferenze.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.SubScene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddProgrammaController implements Initializable {
+    private EditConferenceController editConferenceController;
+    private Conferenza conferenza;
+    private Sessione sessione;
+    private SubScene subScene;
+    private AddSessioneController addSessioneController;
+    @FXML
+    private Button annullaButton;
+    @FXML
+    private Button editEventiButton;
+    @FXML
+    private Button editIntervalliButton;
+    @FXML
+    private Button editInterventiButton;
+    @FXML
+    private ListView<EventoSociale> eventiList;
+    @FXML
+    private ListView<Intervallo> intervalliList;
+    @FXML
+    private ListView<Intervento> interventiList;
+    @FXML
+    private Button fineButton;
+
+    public Conferenza getConferenza() {
+        return conferenza;
+    }
+
+    public void setConferenza(Conferenza conferenza) {
+        this.conferenza = conferenza;
+    }
+
+    @FXML
+    void annullaOnAction(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../FXML/EditConference.fxml"));
+        EditConferenceController controller = new EditConferenceController();
+        controller.setConferenza(conferenza);
+        loader.setController(controller);
+        Parent root = loader.load();
+        subScene.setRoot(root);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void setEditConferenceController(EditConferenceController editConferenceController) {
+        this.editConferenceController=editConferenceController;
+    }
+
+    @FXML
+    void editEventiOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editIntervalliOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editInterventiOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fineButtonOnAction(ActionEvent event) {
+
+    }
+
+    public Sessione getSessione() {
+        return sessione;
+    }
+
+    public void setSessione(Sessione sessione) {
+        this.sessione = sessione;
+    }
+
+    public SubScene getSubScene() {
+        return subScene;
+    }
+
+    public void setSubScene(SubScene subScene) {
+        this.subScene = subScene;
+    }
+
+    public AddSessioneController getAddSessioneController() {
+        return addSessioneController;
+    }
+
+    public void setAddSessioneController(AddSessioneController addSessioneController) {
+        this.addSessioneController = addSessioneController;
+    }
+}
--- Codice/src/View/Controller/AddSessioneController.java
+++ Codice/src/View/Controller/AddSessioneController.java
@@ -2,6 +2,8 @@ package View.Controller;
 
 import Persistence.Entities.Conferenze.Conferenza;
 import Persistence.Entities.Conferenze.Sala;
+import Persistence.Entities.Conferenze.Sessione;
+import Services.Sale;
 import Services.Sessioni;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,11 +18,14 @@ import tornadofx.control.DateTimePicker;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Date;
+import java.sql.Time;
 import java.util.ResourceBundle;
 
 public class AddSessioneController implements Initializable {
     private Conferenza conferenza;
     private Sessioni sessioni;
+    private Sale sale;
     private EditConferenceController editConferenceController;
     private SubScene subscene;
     @FXML
@@ -42,12 +47,44 @@ public class AddSessioneController implements Initializable {
     private ChoiceBox<Sala> saleChoice;
 
     @FXML
-    void nextOnAction(ActionEvent event) {
-
+    void nextOnAction(ActionEvent event) throws IOException {
+        Sessione s = setSessione();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../FXML/AddProgramma.fxml"));
+        AddProgrammaController controller = new AddProgrammaController();
+        loader.setController(controller);
+        controller.setSessione(s);
+        controller.setSubScene(subscene);
+        controller.setConferenza(conferenza);
+        controller.setEditConferenceController(editConferenceController);
+        Parent root = loader.load();
+        subscene.setRoot(root);
     }
+
+    private Sessione setSessione() {
+        Sessione s = new Sessione();
+        s.setTitolo(nomeTF.getText());
+        s.setLocazione(saleChoice.getValue());
+        s.setDataInizio(Date.valueOf(inizioDateTimePicker.getDateTimeValue().toLocalDate()));
+        s.setDataFine(Date.valueOf(fineDateTimePicker.getDateTimeValue().toLocalDate()));
+        s.setOrarioInizio(Time.valueOf(inizioDateTimePicker.getDateTimeValue().toLocalTime()));
+        s.setOrarioFine(Time.valueOf(fineDateTimePicker.getDateTimeValue().toLocalTime()));
+        return  s;
+    }
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        setSale();
+    }
+
+    private void setSale() {
+        try{
+            sale = new Sale(conferenza.getSede());
+            sale.loadSale();
+            saleChoice.setItems(sale.getSale());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void setEditConferenceController(EditConferenceController editConferenceController) {
