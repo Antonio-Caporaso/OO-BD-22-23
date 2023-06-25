02680e7f43f500e53f1a89d6e5a178296733f683
Giordi9902 <giorgio99difusco@gmail.com>
2023-06-25 23:07:04 +0200

    Aggiunta interventi_speaker view

--- Documentazione/Documentazione_DB/Scripts/Views.sql
+++ Documentazione/Documentazione_DB/Scripts/Views.sql
@@ -10,3 +10,10 @@ select s.nome as Sede, count(id_conferenza) as Numero_Conferenze
 from sede s,conferenza c
 where s.id_sede = c.id_sede
 group by s.nome;
+
+-- Vista che mostra il numero di interventi di ogni speaker:
+create view interventi_speaker as
+select s.nome || ' ' || s.cognome as Speaker, count(i.id_intervento)
+from speaker s, intervento i 
+where s.id_speaker = i.id_speaker
+group by s.nome,s.cognome;
