e2bca2119250cea30963bec58bf97c03e4fd911f
Giorgio Di Fusco <giorgio99difusco@gmail.com>
2023-07-08 11:13:25 +0200

    Rimozine spazio

--- Codice/src/Utilities/Sponsors.java
+++ Codice/src/Utilities/Sponsors.java
@@ -1,11 +1,11 @@
 package Utilities;
-
 import Persistence.DAO.SponsorDao;
 import Persistence.Entities.organizzazione.Sponsor;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-public class Sponsors {
+public class Sponsors
+{
     private ObservableList<Sponsor> sponsors;
     public Sponsors(){
         sponsors = FXCollections.observableArrayList();
@@ -18,7 +18,6 @@ public class Sponsors {
     public ObservableList<Sponsor> getSponsors() {
         return sponsors;
     }
-
     public void setSponsors(ObservableList<Sponsor> sponsors) {
         this.sponsors = sponsors;
     }
