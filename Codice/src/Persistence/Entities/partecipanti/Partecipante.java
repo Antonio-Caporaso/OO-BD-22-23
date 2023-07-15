9ef599294f660753d7ac5a8f73b7223f2de520c0
Giorgio Di Fusco <giorgio99difusco@gmail.com>
2023-07-15 12:00:01 +0200

    Fix toString in Partecipante.java

--- Codice/src/Persistence/Entities/partecipanti/Partecipante.java
+++ Codice/src/Persistence/Entities/partecipanti/Partecipante.java
@@ -69,6 +69,10 @@ public class Partecipante {
     public void setEmail(String email) {
         this.email = email;
     }
+    @Override
+    public String toString(){
+        return nome + " " + cognome;
+    }
 
     @Override
     public boolean equals(Object o) {
