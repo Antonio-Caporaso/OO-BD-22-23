482c226d4b24b9b1a65f2035c125823e973090ab
Giorgio Di Fusco <giorgio99difusco@gmail.com>
2023-07-16 20:12:48 +0200

    Aggiunta stringa più dettagliata

--- Codice/src/View/Controller/ModificaDettagliSessioneController.java
+++ Codice/src/View/Controller/ModificaDettagliSessioneController.java
@@ -78,8 +78,8 @@ public class ModificaDettagliSessioneController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        toolTipInizioConferenza.setText(conferenza.getInizio().toString());
-        toolTipFineConferenza.setText(conferenza.getFine().toString());
+        toolTipInizioConferenza.setText("La conferenza inizia il giorno "+conferenza.getInizio().getDate()+" alle "+conferenza.getInizio().getTime());
+        toolTipFineConferenza.setText("La conferenza finisce il giorno "+conferenza.getFine().getDate()+" alle "+conferenza.getFine().getTime());
         saleChoice.setValue(sessione.getLocazione());
         try {
             setTitoloSessione();
