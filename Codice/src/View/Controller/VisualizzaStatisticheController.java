5d3c00e92f74df87e7a432ba925d3dcbaf040f56
Giorgio Di Fusco <giorgio99difusco@gmail.com>
2023-07-17 20:14:42 +0200

    Aggiunta finestre per statistiche mensili e annuali

--- Codice/src/View/Controller/VisualizzaStatisticheController.java
+++ Codice/src/View/Controller/VisualizzaStatisticheController.java
@@ -9,28 +9,31 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
-import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
-import java.sql.Date;
 import java.sql.SQLException;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
-import java.time.Year;
 import java.util.LinkedList;
 import java.util.ResourceBundle;
 
 public class VisualizzaStatisticheController implements Initializable {
     @FXML
-    private Pane currentMonthPieChartPanel;
-
+    private BorderPane currentMonthPieChartPanel;
     @FXML
-    private Pane currentYearPieChartPane;
+    private BorderPane currentYearPieChartPane;
 
     @FXML
     private Label totalConferenzeLabel;
@@ -48,7 +51,54 @@ public class VisualizzaStatisticheController implements Initializable {
         setLabels();
     }
 
-    private void setCurrentYearPieChart(){
+    private void createPieChart(ObservableList<Stats> stats, ObservableList<PieChart.Data> pieChartData, BorderPane pane, String title) {
+        for (Stats dataPoint : stats) {
+            PieChart.Data data = new PieChart.Data(dataPoint.getIstituzione(), dataPoint.getPercentuale());
+            pieChartData.add(data);
+        }
+        PieChart pieChart = new PieChart(pieChartData);
+        pane.setCenter(pieChart);
+        pieChart.setMaxHeight(247);
+        pieChart.setMaxWidth(247);
+        pieChart.setLabelsVisible(true);
+        pieChart.setTitle(title);
+        pieChart.setClockwise(true);
+        pieChart.setLabelLineLength(10); // Adjust the length of the label lines as needed
+        for (PieChart.Data data : pieChart.getData()) {
+            data.setName(data.getName() + " (" + Math.round(data.getPieValue()) + "%)");
+        }
+        pieChart.setLabelsVisible(true);
+        pieChart.setStartAngle(180);
+    }
+
+    private LinkedList<Stats> retrieveIstituzioniByMonth(int m, int y) throws SQLException {
+        InterventoDao dao = new InterventoDao();
+        return dao.retrieveInterventiStatsByMonth(m, y);
+    }
+
+    private LinkedList<Stats> retrieveIstituzioniByYear(int i) throws SQLException {
+        InterventoDao dao = new InterventoDao();
+        return dao.retrieveInterventiStatsByYear(i);
+    }
+
+    private void setCurrentMonthPieChart() {
+        Month mese = LocalDate.now().getMonth();
+        int anno = LocalDate.now().getYear();
+        try {
+            ObservableList<Stats> stats = FXCollections.observableArrayList();
+            stats.addAll(retrieveIstituzioniByMonth(mese.getValue(), anno));
+            ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+            if (stats.isEmpty()) {
+                showAlert(Alert.AlertType.INFORMATION, "Non risultano interventi nel mese cercato");
+            } else {
+                createPieChart(stats, pieChartData, currentMonthPieChartPanel, "Statistica mensile");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setCurrentYearPieChart() {
         int anno = LocalDate.now().getYear();
         try {
             ObservableList<Stats> stats = FXCollections.observableArrayList();
@@ -59,23 +109,6 @@ public class VisualizzaStatisticheController implements Initializable {
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    private void setCurrentMonthPieChart() {
-        Month mese = LocalDate.now().getMonth();
-        int anno = LocalDate.now().getYear();
-        try {
-            ObservableList<Stats> stats = FXCollections.observableArrayList();
-            stats.addAll(retrieveIstituzioniByMonth(mese.getValue(),anno));
-            ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
-            if (stats.isEmpty()) {
-                showAlert(Alert.AlertType.INFORMATION, "Non risultano interventi nel mese cercato");
-            } else {
-                createPieChart(stats, pieChartData, currentMonthPieChartPanel, "Statistica mensile");
-            }
-        }catch (SQLException e){
-            e.printStackTrace();
         }
     }
 
@@ -92,37 +125,29 @@ public class VisualizzaStatisticheController implements Initializable {
         }
     }
 
-    private LinkedList<Stats> retrieveIstituzioniByMonth(int m, int y) throws SQLException {
-        InterventoDao dao = new InterventoDao();
-        return dao.retrieveInterventiStatsByMonth(m, y);
-    }
-
-    private LinkedList<Stats> retrieveIstituzioniByYear(int i) throws SQLException {
-        InterventoDao dao = new InterventoDao();
-        return dao.retrieveInterventiStatsByYear(i);
-    }
-
-    private void createPieChart(ObservableList<Stats> stats, ObservableList<PieChart.Data> pieChartData, Pane pane, String title) {
-        for (Stats dataPoint : stats) {
-            PieChart.Data data = new PieChart.Data(dataPoint.getIstituzione(), dataPoint.getPercentuale());
-            pieChartData.add(data);
-        }
-        PieChart pieChart = new PieChart(pieChartData);
-        pane.getChildren().add(pieChart);
-        pieChart.setMaxHeight(247);
-        pieChart.setMaxWidth(247);
-        pieChart.setLabelsVisible(true);
-        pieChart.setTitle(title);
-        pieChart.setClockwise(true);
-        pieChart.setLabelLineLength(10); // Adjust the length of the label lines as needed
-        pieChart.getData().forEach(data -> data.setName((String.valueOf(data.getPieValue()))));
-        pieChart.setLabelsVisible(true);
-        pieChart.setStartAngle(180);
-    }
-
     private static void showAlert(Alert.AlertType information, String s) {
         Alert alert = new Alert(information);
         alert.setContentText(s);
         alert.showAndWait();
+    }
+
+    @FXML
+    void openStatisticheAnnualiButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openStatisticheMensiliWindow(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../FXML/StatisticheMensili.fxml"));
+        MonthlyStatWindowController controller = new MonthlyStatWindowController();
+        loader.setController(controller);
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setTitle("Calcola statistiche mensili");
+        stage.setResizable(false);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.show();
     }
 }
---
+++ Codice/src/View/Controller/YearlyStatWindowController.java
@@ -0,0 +1,82 @@
+package View.Controller;
+
+import Persistence.DAO.InterventoDao;
+import Utilities.Stats;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.layout.BorderPane;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.ResourceBundle;
+
+public class YearlyStatWindowController implements Initializable {
+    @FXML
+    private Spinner<Integer> annoSpinner;
+    @FXML
+    private BorderPane pieChartPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setSpinners();
+    }
+
+    private void setSpinners() {
+        SpinnerValueFactory<Integer> anniValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(2023,3000,1,1);
+        annoSpinner.setValueFactory(anniValueFactory);
+    }
+    private static void showAlert(Alert.AlertType information, String s) {
+        Alert alert = new Alert(information);
+        alert.setContentText(s);
+        alert.showAndWait();
+    }
+    private void createPieChart(ObservableList<Stats> stats, ObservableList<PieChart.Data> pieChartData, BorderPane pane, String title) {
+        for (Stats dataPoint : stats) {
+            PieChart.Data data = new PieChart.Data(dataPoint.getIstituzione(), dataPoint.getPercentuale());
+            pieChartData.add(data);
+        }
+        PieChart pieChart = new PieChart(pieChartData);
+        pane.setCenter(pieChart);
+        pieChart.setMaxHeight(260);
+        pieChart.setMaxWidth(260);
+        pieChart.setLabelsVisible(true);
+        pieChart.setLabelLineLength(10);
+        for (PieChart.Data data : pieChart.getData()) {
+            data.setName(data.getName() + " (" + Math.round(data.getPieValue()) + "%)");
+        }
+        pieChart.setTitle(title);
+        pieChart.setClockwise(true);
+        pieChart.setLabelsVisible(true);
+        pieChart.setStartAngle(180);
+    }
+    private LinkedList<Stats> retrieveIstituzioniByYear(int i) throws SQLException {
+        InterventoDao dao = new InterventoDao();
+        return dao.retrieveInterventiStatsByYear(i);
+    }
+    @FXML
+    void calcolaGrafico(ActionEvent event) {
+        try {
+            int anno = annoSpinner.getValue();
+            ObservableList<Stats> stats = FXCollections.observableArrayList();
+            stats.addAll(retrieveIstituzioniByYear(anno));
+            ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+            if (stats.isEmpty()) {
+                showAlert(Alert.AlertType.INFORMATION, "Non risultano interventi nel mese cercato");
+            } else {
+                createPieChart(stats, pieChartData, pieChartPane, "Statistica mensile");
+            }
+        } catch (InputMismatchException e) {
+            showAlert(Alert.AlertType.ERROR, "Inserire un input valido");
+        } catch (SQLException e) {
+            showAlert(Alert.AlertType.ERROR, e.getMessage());
+        }
+    }
+}
