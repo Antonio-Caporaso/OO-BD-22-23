70cea1473e7110626c4ab06378d46a53a853b689
Giordi9902 <giorgio99difusco@gmail.com>
2023-10-11 16:30:19 +0200

    Aggiornamento readme e rimozione link a server aws dbconnection

--- Codice/src/Model/DbConfig/DBConnection.java
+++ Codice/src/Model/DbConfig/DBConnection.java
@@ -14,11 +14,12 @@ public class DBConnection {
         return dbcon;
     }
     public Connection getConnection(){
-        String pwd = "mandarino";
+        
         try{
             if(conn == null || conn.isClosed()){
                 Class.forName("org.postgresql.Driver");
-                conn = DriverManager.getConnection("jdbc:postgresql://database-oobd.ct0ckvilfzkl.eu-central-1.rds.amazonaws.com:5432/OO_DB?currentSchema=conference","Master_User",pwd); // Connessione server AWS
+                // Inserire indirizzo del database, nome utente e password
+                conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ProgettoOOP", "postgres", "postgres");
             }
         }catch (SQLException | ClassNotFoundException throwables){
             throwables.printStackTrace();
--- README.md
+++
@@ -1,2 +0,0 @@
-# OO-BD-22-23
-Progetto di Object Orientation e Basi di Dati 22-23 UNINA
